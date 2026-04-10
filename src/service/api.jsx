2b48dfa4277c28868e8cefa83d83512e193eb5f1
IanAlfaroSantos <ialfasa2020@gmail.com>
2026-04-10 16:29:48 -0600

    Version 2

--- src/service/api.jsx
+++ src/service/api.jsx
@@ -1,12 +1,12 @@
-import api from '../service/api';
+import axios from "axios";
 
 const baseURL = import.meta.env.VITE_URL_BACKEND;
 
-const api = api.create({
+const api = axios.create({
   baseURL,
   headers: {
-    'Content-Type': 'application/json'
-  }
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
